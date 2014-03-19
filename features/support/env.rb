95582ffc448a2d32523809c7b94a88fc30c20bb1
Tom Stuart <tom@codon.com>
2014-03-19 23:53:10 +0000

    Silence RSpec matcher deprecation warnings
    These are coming from Capybara, so there's nothing I can do about it.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,7 @@
 require 'capybara/cucumber'
 Capybara.app, _ = Rack::Builder.parse_file(File.expand_path('../../../config.ru', __FILE__))
+
+require 'rspec'
+RSpec.configure do |config|
+  config.deprecation_stream = '/dev/null'
+end
