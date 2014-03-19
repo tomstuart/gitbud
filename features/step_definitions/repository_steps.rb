4d61265efb1cca28a00d496941ac7227cfad74cd
Tom Stuart <tom@codon.com>
2014-03-19 23:53:52 +0000

    Add step for seeing repository branches

--- features/step_definitions/repository_steps.rb
+++ features/step_definitions/repository_steps.rb
@@ -3,3 +3,8 @@ When(/^I enter the Git URL of a public repository$/) do
   fill_in 'Repository URL', with: 'git://github.com/git/git.git'
   click_on 'View repository'
 end
+
+Then(/^I see a list of its branches$/) do
+  branch_names = all('.branches > *').map(&:text)
+  expect(branch_names).to contain_exactly 'maint', 'master', 'next', 'pu', 'todo'
+end
